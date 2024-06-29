0d353e1bcd65ef004e52bec45f22085965158510
IanTapply22 <contact@ian-tapply.me>
2024-06-28 23:26:35 -0400

    Add more buttons and functionality to task creator

--- src/components/TaskCreator.vue
+++ src/components/TaskCreator.vue
@@ -2,22 +2,70 @@
     <v-row>
         <v-col cols="12">
             <h2 class="text-left pb-2">Task Creator</h2>
-            <v-btn class="d-flex justify-start mb-2" color="green" @click="taskCount++">Create Task</v-btn>
+            <v-btn class="d-flex justify-start mb-4" color="green" @click="addTask">Create Task</v-btn>
 
-            <div v-for="(task, index) in taskCount" :key="index">
-                <v-text-field label="Task Description" ></v-text-field>
-                <v-text-field label="Subtask Description" ></v-text-field>
+            <div v-for="(task, index) in tasks" :key="index" class="mt-3">
+                <v-card>
+                    <v-card-title>
+                        <h3>{{ shortenDescription(task.description) }}</h3>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-row>
+                            <v-col cols="12" md="6">
+                                <v-text-field label="Task Description" outlined v-model="task.description" clearable></v-text-field>
+                            </v-col>
+                            <v-col cols="12" md="6">
+                                <v-select label="Task Type" outlined :items="taskTypes" v-model="task.type"></v-select>
+                            </v-col>
+                        </v-row>
+                        <v-btn class="d-flex justify-start mb-2" color="blue" @click="addSubdescription(index)">Add Subdescription</v-btn>
+                        <div v-for="(sub, subIndex) in task.subdescriptions" :key="`sub-${subIndex}`">
+                            <v-text-field label="Subdescription" outlined v-model="sub.description"></v-text-field>
+                        </div>
+                    </v-card-text>
+                </v-card>
             </div>
         </v-col>
     </v-row>
 </template>
 
 <script>
+import { TaskType } from '../types/TaskType';
+
 export default {
     data() {
         return {
-            taskCount: 1,
+            tasks: [
+                {
+                    description: '',
+                    type: null,
+                    subdescriptions: [],
+                },
+            ],
+            taskTypes: Object.keys(TaskType).map(key => ({
+                title: key,
+            })),
         };
+    },
+    methods: {
+        addTask() {
+            this.tasks.push({
+                title: '',
+                type: null,
+                subdescriptions: [],
+            });
+        },
+        addSubdescription(taskIndex) {
+            this.tasks[taskIndex].subdescriptions.push({
+                description: '',
+            });
+        },
+        shortenDescription(description) {
+            if (description.length > 20) {
+                return `${description.substring(0, 20)}...`;
+            }
+            return description;
+        },
     },
 };
 </script>
